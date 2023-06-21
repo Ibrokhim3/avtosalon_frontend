cb755f45ba018b8d8f7860225b9cbbd712bc2206
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-06-21 17:14:59 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import { ConfigRoutes } from "./routes/config-routes";
 
 function App() {
+  let hours = 5;
+  let now = new Date().getTime();
+  let setupTime = localStorage.getItem("setupTime");
+  if (setupTime == null) {
+    localStorage.setItem("setupTime", now);
+  } else {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
+      localStorage.clear();
+      localStorage.setItem("setupTime", now);
+    }
+  }
   return (
     <div className="App">
       <ConfigRoutes></ConfigRoutes>
